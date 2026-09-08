d0b8f9e51bc9ca5a3f2456ee8ace1e873c3a8930
mohamadreza <mohamadrezadoustii84@gmail.com>
2026-09-08 18:08:50 +0330

    feat(django):set app setting for installed_apps

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "USER":"root",
+        "PASSWORD":"1234",
+        "HOST":"localhost",
+        "PORT":"3306",
     }
 }
 
