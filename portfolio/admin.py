21f1cf207829e48c9b2d4a899ed125e788532ffe
mohamadreza <mohamadrezadoustii84@gmail.com>
2026-09-20 19:38:37 +0330

    feat(portfolio):update admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Skill, Project, Language, Education, Experience
+from .models import Skill, Project, Language, Education, Experience, ProjectSkill
+
+class ProjectSkillInline(admin.TabularInline):
+    model = ProjectSkill
+    extra = 1
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectSkillInline]
 
 admin.site.register(Skill)
-admin.site.register(Project)
 admin.site.register(Language)
 admin.site.register(Education)
 admin.site.register(Experience)
