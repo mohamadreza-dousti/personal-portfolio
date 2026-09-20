70f7121825bf0024bdb79e7d15ca057c81901a60
mohamadreza <mohamadrezadoustii84@gmail.com>
2026-09-20 19:37:33 +0330

    feat(django)set setting to add templates & static folder

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 
 
 # Email
